3f568b09ab6b924ffd9036f5b43762f101cfbbde
SK <senthilptj@gmail.com>
2022-03-17 19:05:20 +0530

    Address Book - IO

---
+++ src/test/java/com/bridgelabz/d27/AddressBookMainTest.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.d27;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressBookMainTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("File I/O");
+        assertTrue( true );
+    }
+}
